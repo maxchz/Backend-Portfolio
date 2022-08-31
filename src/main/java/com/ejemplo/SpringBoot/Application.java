cee87848c6411b0d7c3fb7d2e7d49d9880486933
Max ChZ <chzanibal@hotmail.com>
2022-08-31 20:54:27 -0300

    corección cors 5

--- src/main/java/com/ejemplo/SpringBoot/Application.java
+++ src/main/java/com/ejemplo/SpringBoot/Application.java
@@ -3,6 +3,7 @@ package com.ejemplo.SpringBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,14 +11,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }        
+    }
     
-    @Bean
+    
+    
+    
+   @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("https://app-portfolio-front-argpro.web.app").allowedMethods("*").allowedHeaders("*");
+		registry.addMapping("/**")
+                        .allowedOrigins("https://app-portfolio-front-argpro.web.app")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(false)
+                        .maxAge(-1);
                 }
         };
     }
