ac313b3a2d8b4d04cd3821ed68927a1bd039321a
Max ChZ <chzanibal@hotmail.com>
2022-09-01 09:47:58 -0300

    corrección cors 30

--- src/main/java/com/ejemplo/SpringBoot/Cors/CorsConfig.java
+++ src/main/java/com/ejemplo/SpringBoot/Cors/CorsConfig.java
@@ -1,13 +1,26 @@
 
 package com.ejemplo.SpringBoot.Cors;
 
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+	CorsConfiguration configuration = new CorsConfiguration();
+	configuration.setAllowedOrigins(Arrays.asList("https://app-portfolio-front-argpro.web.app"));
+	configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	source.registerCorsConfiguration("/**", configuration);
+	return source;
+}
     
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -20,5 +33,6 @@ public class CorsConfig implements WebMvcConfigurer {
 			
             }
         };
-    }    
+    }  
+    
 }
