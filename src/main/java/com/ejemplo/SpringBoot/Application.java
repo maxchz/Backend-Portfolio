5dd51eced09c177a41da38412e5be6d4ffab771c
Max ChZ <chzanibal@hotmail.com>
2022-09-01 21:39:41 -0300

    corrección cors 35

--- src/main/java/com/ejemplo/SpringBoot/Application.java
+++ src/main/java/com/ejemplo/SpringBoot/Application.java
@@ -20,7 +20,12 @@ public class Application {
     
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {};
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("https://app-portfolio-front-argpro.web.app");
+            }
+        };
 
     }
         /*return new WebMvcConfigurerAdapter() {
