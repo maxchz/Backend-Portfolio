7f562511f99021c3aadcd811ac451353c32029ed
Max ChZ <chzanibal@hotmail.com>
2022-09-03 19:21:07 -0300

    cambia generation type en model persona a Identity

--- src/main/java/com/ejemplo/SpringBoot/model/Persona.java
+++ src/main/java/com/ejemplo/SpringBoot/model/Persona.java
@@ -12,7 +12,7 @@ import lombok.Setter;
 @Entity
 public class Persona {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;   
     private String nombre;   
     private String apellido;
