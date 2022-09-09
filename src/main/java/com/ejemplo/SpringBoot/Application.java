c0110988ceef771135621e80ee8445ec239384e0
Max ChZ <chzanibal@hotmail.com>
2022-09-08 22:54:59 -0300

    cambia repository param_var

--- src/main/java/com/ejemplo/SpringBoot/Application.java
+++ src/main/java/com/ejemplo/SpringBoot/Application.java
@@ -31,29 +31,7 @@ public class Application {
             }
         };
 
-    }
-        /*return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("https://app-portfolio-front-argpro.web.app");
-            }
-        };
-    }*/
-    
-    /*@Bean
-    public CorsFilter corsFilter(){
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(Arrays.asList("https://app-portfolio-front-argpro.web.app"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
-        configuration.setAllowedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Authorization","Content-Type","Accept","X-Requested-With","Access-Control-Request-Method","Access-Control-Request-Headers"));
-        configuration.setExposedHeaders(Arrays.asList("Origin","Content-Type","Accept","Authorization","Access-Control-Allow-Origin","Access-Control-Allow-Origin","Access-Control-Allow-Credentials"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return  new CorsFilter(source);
-    }*/
-
-    
+    }   
     
 }
 
